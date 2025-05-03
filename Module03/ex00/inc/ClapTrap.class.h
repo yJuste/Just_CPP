49459e6981b25a55bbefed81e567f55024358394
justJuste <jules.longin1@gmail.com>
2025-05-03 03:30:22 +0200

    add base for module03

---
+++ Module03/ex00/inc/ClapTrap.class.h
@@ -0,0 +1,29 @@
+// ************************************************************************** //
+//                                                                            //
+//                ClapTrap.class.h                                            //
+//                Created on  : xxx May xx xx:xx:xx 2025                      //
+//                Last update : xxx May xx xx:xx:xx 2025                      //
+//                Made by     : Juste - Jules Longin                          //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef CLAPTRAP_CLASS_H
+# define CLAPTRAP_CLASS_H
+
+// Standard Libraries
+
+# include <iostream>
+
+// ************************************************************************** //
+//                                ClapTrap Class                              //
+// ************************************************************************** //
+
+class	ClapTrap
+{
+	private:
+
+	public:
+
+};
+
+#endif
---
+++ Module03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,16 @@
+// ************************************************************************** //
+//                                                                            //
+//                ClapTrap.cpp                                                //
+//                Created on  : xxx May xx xx:xx:xx 2025                      //
+//                Last update : xxx May xx xx:xx:xx 2025                      //
+//                Made by     : Juste - Jules Longin                          //
+//                                                                            //
+// ************************************************************************** //
+
+#include "ClapTrap.class.h"
+
+// ~Structor
+
+// Operator Overload
+
+// Getter
