a2690db5a250bedf8f14381eb3bc172bbe694f50
justJuste <jules.longin1@gmail.com>
2025-05-04 14:29:51 +0200

    add base for mod04

---
+++ Module04/ex00/src/X.cpp
@@ -0,0 +1,18 @@
+// ************************************************************************** //
+//                                                                            //
+//                X.cpp                                                       //
+//                Created on  : xxx May xx xx:xx:xx 2025                      //
+//                Last update : xxx May xx xx:xx:xx 2025                      //
+//                Made by     : Juste - Jules Longin                          //
+//                                                                            //
+// ************************************************************************** //
+
+#include "X.class.h"
+
+// ~Structor
+
+// Operator Overload
+
+// Methode
+
+// Getter
