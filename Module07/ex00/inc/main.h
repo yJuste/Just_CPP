988e5500db1683745ebfc59ff8aab4ccd23c0d42
justJuste <jules.longin1@gmail.com>
2025-05-09 23:17:49 +0200

    add ex00 base = mod7 - no compilation

---
+++ Module07/ex00/inc/main.h
@@ -0,0 +1,22 @@
+// ************************************************************************** //
+//                                                                            //
+//                main.h                                                      //
+//                Created on  : xxx May xx xx:xx:xx 2025                      //
+//                Last update : xxx May xx xx:xx:xx 2025                      //
+//                Made by     : Juste - Jules Longin                          //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef MAIN_H
+# define MAIN_H
+
+// Standard Libraries
+
+# include <iostream>
+# include <Array.hpp>
+
+// Defines
+
+# define MAX_VAL 750
+
+#endif
